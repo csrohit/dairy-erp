300acd138c65daa741232feb595ff73b28f52134
Rohit Nimkar <nehalnimkar@gmail.com>
2020-07-27 10:37:18 +0530

    add payment routes

--- routes/index.mjs
+++ routes/index.mjs
@@ -3,12 +3,14 @@ import express from 'express'
 import userRoute from './controllers/userController.mjs';
 import dairyRoute from './controllers/dairyController.mjs';
 import readingRoute from './controllers/readingController.mjs';
+import paymentRoute from './controllers/paymentController.mjs';
 
 const index = express.Router();
 
 index.use('/user', userRoute);
 index.use('/dairy', dairyRoute);
 index.use('/reading', readingRoute);
+index.use('/payment', paymentRoute);
 
 
 index.get('/', (req, res) => {
